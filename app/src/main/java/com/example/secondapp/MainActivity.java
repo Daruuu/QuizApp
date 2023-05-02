16b444e5d467c2af17a26560c5656918add8de7f
Daruuu <thedaruny16@gmail.com>
2023-05-02 20:47:13 +0200

    adding new activity result

--- app/src/main/java/com/example/secondapp/MainActivity.java
+++ app/src/main/java/com/example/secondapp/MainActivity.java
@@ -21,30 +21,22 @@ public class MainActivity extends AppCompatActivity {
     Button buttonTrue;
     Button buttonFalse;
     TextView textViewOne;
-    ImageButton imageButtonPrev;
-    ImageButton imageButtonNext;
     private List<Question> questionList;
     private Set<Integer> saveQuestionsIndex;
-    public Intent intentQuizFinish;
-
-    //    private String answer;
     private int currentQuestionIndex = 0;
+    private int totalPointsScore = 0;
 
-    private int correctAnswerCount = 0;
-
-    //llamamos a este metodo cuando creamos una instancia de la clase activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         fillQuestionsWithData();
         saveQuestionsIndex = new HashSet<>();
 
         buttonTrue = findViewById(R.id.true_button);
         buttonFalse = findViewById(R.id.false_button);
         textViewOne = findViewById(R.id.textView01);
-        imageButtonPrev = findViewById(R.id.prev_button);
-        imageButtonNext = findViewById(R.id.next_button);
 
         buttonTrue.setBackgroundColor(Color.rgb(120, 194, 87));
         buttonTrue.setOnClickListener(v -> {
@@ -54,7 +46,6 @@ public class MainActivity extends AppCompatActivity {
         buttonFalse.setOnClickListener(v -> {
             checkQuestion(false);
         });
-
         updateQuestion();
     }
 
@@ -73,7 +64,6 @@ public class MainActivity extends AppCompatActivity {
         questionList.add(new Question("El Monte Everest es la montanya más alta del mundo" + questionMark, true));
     }
 
-    // quiero que las preguntas no siempre empiezen por el index del arraylist
     void updateQuestion() {
         Random random = new Random();
         int randomIndex;
@@ -81,54 +71,41 @@ public class MainActivity extends AppCompatActivity {
         do {
             randomIndex = random.nextInt(questionList.size());
         } while (saveQuestionsIndex.contains(randomIndex));
-
         saveQuestionsIndex.add(randomIndex);
         currentQuestionIndex = randomIndex;
 
         Question myQuestion = questionList.get(currentQuestionIndex);
         String questionTitle = myQuestion.getTitleQuestion();
         textViewOne.setText(questionTitle);
-/*
-        if (saveQuestionsIndex.size() == questionList.size()) {
-            saveQuestionsIndex.clear();
-        }
-*/
     }
 
     void checkQuestion(boolean userAnswer) {
-        // creamos una nueva actividad
-
-        //compruebo si la respuesta es correcta, comparando la respuesta real con la repsuesta del usuario
         if (questionList.get(currentQuestionIndex).getAnswer() == userAnswer) {
             showToastAtBottom(R.string.correct_toast);
-            //si es correcta aumento el contador correcrtAnserCount
-            correctAnswerCount++;
+            totalPointsScore++;
         } else {
             showToastAtBottom(R.string.incorrect_toast);
         }
-
-        if (currentQuestionIndex == questionList.size()) {
-/*
-            String strPointsQuiz = "Has obtained a total points of " + correctAnswerCount + " points.";
-            textViewOne.setText(strPointsQuiz);
-*/
-            correctAnswerCount = 0;
-            currentQuestionIndex = 0;
-        }
+//        if (currentQuestionIndex == questionList.size()) {
+//            totalPointsScore = 0;
+//            currentQuestionIndex = 0;
+//        }
         updateQuestion();
     }
 
     void showToastAtBottom(int toastMessage) {
         Toast t;
-        // horizontal y vertical
         t = Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT);
-//        t.setGravity(Gravity.TOP, 0, 0);
         t.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 0);
         t.show();
     }
+
+    void moveToResultScreen() {
+//        int score = totalPointsScore;
+        Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra("score", totalPointsScore);
+        startActivity(intent);
+        //pasar informacion hacia el nuevo layout
+
+    }
 }
-/*
-tengo mi aplicacion que trata de responder 10 preguntas y obtener un resultado de las preguntas correctas, ahora mo que me falta agregar es un nuveo LinearLayout para poner la puntiacion que he obtenido; dado este codigo como puedo realizarlo
-
-
- */
---
+++ app/src/main/java/com/example/secondapp/ResultActivity.java
@@ -0,0 +1,33 @@
+package com.example.secondapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ResultActivity extends AppCompatActivity {
+
+    TextView textViewFinishQuiz;
+    Button buttonRestart;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        textViewFinishQuiz = findViewById(R.id.TextViewFinishQuiz);
+        buttonRestart = findViewById(R.id.restart_game_button);
+        buttonRestart.setOnClickListener(v -> {
+            endActivity();
+        });
+        int scoreTotal = getIntent().getIntExtra("score", 0);
+
+    }
+
+    void endActivity() {
+        finish();
+    }
+}
